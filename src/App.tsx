1d13a98f79a2b3471597da0f7520bcfd3187e106
Michał Żuk <m.zuk@neurosys.com>
2022-10-10 13:17:50 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import {} from "@headlessui/react";
+import useZustandStore from "./zustandStore";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  // const { addListItem, list } = useZustandStore((store) => store);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* {list.map((item) => (
+        <p>{item}</p>
+      ))}
+      <hr />
+      <input
+        type="text"
+        value={inputValue}
+        onChange={({ target: { value } }) => setInputValue(value)}
+      />
+      <button
+        onClick={() => {
+          addListItem(inputValue);
+          setInputValue("");
+        }}
+      >
+        ADD
+      </button> */}
+      <div style={{ margin: 16 }}>
+        <button style={{ color: "red" }}>Clear</button>
+      </div>
     </div>
   );
 }
